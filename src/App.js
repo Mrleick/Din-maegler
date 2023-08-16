3d1041743915fce4a60afe2a957672e8dd8c7762
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-08-16 11:41:44 +0200

    Fixed code.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import logoImage from "./assets/logo.png";
+import Info from "./components/info";
+import Navi from "./components/navigation";
 
-// Styled-components
-const NavBar = styled.div`
-  display: flex;
-  height: 90px;
-  align-items: center;
+// Styled-component for the Main container
+const Main = styled.div`
+  max-width: 1110px;
+  margin: 0 auto;
 `;
 
-const LogoImage = styled.img`
-  width: 269px;
-  height: 49px;
-  cursor: pointer;
-`;
-
-const NavList = styled.ul`
-  display: flex;
-  gap: 40px;
-  list-style: none;
-  flex: 6;
-  justify-content: flex-end;
-`;
-
-const NavA = styled.a`
-  text-decoration: none;
-  color: black;
-`;
-
-function NavItem({ label, link }) {
+function App() {
   return (
-    <NavItem>
-      <NavA href={link}>{label}</NavA>
-    </NavItem>
+    <div>
+      <Info />
+      <Main>
+        <Navi />
+      </Main>
+    </div>
   );
 }
 
-function Navigation() {
-  return (
-    <NavBar>
-      <LogoImage src={logoImage} alt="Din mægler logo" />
-      <NavList>
-        <NavItem label="Boliger til salg" link="/" />
-        <NavItem label="Mæglere" link="/om-os" />
-        <NavItem label="Mine favoritter" link="/kontakt" />
-        <NavItem label="Kontakt os" link="/kontakt" />
-      </NavList>
-    </NavBar>
-  );
-}
-
-export default Navigation;
+export default App;
