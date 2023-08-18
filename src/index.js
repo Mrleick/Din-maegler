84ea6d7e24277dac60dcc7d96bab96dbc12c72df
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-08-18 08:33:53 +0200

    Added routing.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App";
+import Home from "./pages/home";
+import "./Index.css";
 
-const root = document.getElementById("root");
+const root = createRoot(document.getElementById("root"));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  root
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
