3a19f488c2f26ec67259fc48e1bd43a63fa94533
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-09-01 14:03:37 +0200

    Created agent card.

--- src/components/AgentCard.jsx
+++ src/components/AgentCard.jsx
@@ -1,10 +1,16 @@
+import { FaEnvelope } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 const AgentCard = ({ agent }) => {
   return (
-    <div className="text-center max-w-[350px]">
+    <div className="text-center max-w-[350px] rounded border border-agentbordercolor shadow-lg shadow-agentshadow">
       <img src={agent.image.url} alt="" />
-      <p className="text-[24px] font-medium pt-[26px]">{agent.name}</p>
-      <p className="text-[18px] text-para02">{agent.email}</p>
-      {/* Other agent information */}
+        <p className="text-[24px] font-medium pt-[26px]">{agent.name}</p>
+        <p className="text-[18px] text-para02">{agent.email}</p>
+        <div className="flex justify-center gap-[16px] pt-[16px] pb-[26px] ">
+          <FaEnvelope className="block" />
+          <FaLinkedinIn className="block" />
+      </div>
     </div>
   );
 };
